b5e0d28681fe59acb0d4e1a97c762f794a30a3c9
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-06-12 21:27:55 +0100

    Remove unused imports

--- src/MainBody.js
+++ src/MainBody.js
@@ -2,8 +2,6 @@ import React from 'react'
 import SideMenu from './SideMenu'
 import Videos from './Videos'
 import './MainBody.css'
-import { Icon } from '@material-ui/core'
-import AcUnitIcon from '@material-ui/icons/AcUnit';
 
 function MainBody() {
     return (
