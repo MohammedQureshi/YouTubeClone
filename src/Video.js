514bcaef46c2ae52c333f2a6b03b76c47fea579f
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-06-13 13:28:23 +0100

    Final cleanup and adding variety

--- src/Video.js
+++ src/Video.js
@@ -6,11 +6,11 @@ function Video({Avatar, ThumbNail, Title, Channel, Views, Time}) {
     return (
         <Button className="Video__Clickable">
             <div className="Video">
-                <img src={ThumbNail} />
+                <img src={ThumbNail} alt="ThumbNail" />
                 {/* Avatar */}
                 <div className="Video__Details">
                     <div class="Video__ProfilePicture">
-                        <img src={Avatar} />
+                        <img src={Avatar} alt="Avatar" />
                     </div>
                     <div class="Video__Right">
                         <h3>{Title}</h3>
