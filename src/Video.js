41bb8c449430eda755b76fcea53284843a761647
Mohammed Faisal Qureshi <mohammedqureshi2998@gmail.com>
2021-06-13 11:43:29 +0100

    Add topic bar at top of videos

--- src/Video.js
+++ src/Video.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import './Video.css'
+import Button from '@material-ui/core/Button'
 
 function Video({Avatar, ThumbNail, Title, Channel, Views, Time}) {
     return (
-        <div className="Video">
+        <Button className="Video__Clickable">
+            <div className="Video">
                 <img src="https://i.ytimg.com/vi/2Bw5f4vYL98/hq720.jpg" />
-            {/* Avatar */}
-            <div className="Video__Details">
-                <div class="Video__ProfilePicture">
-                    <img src="https://yt3.ggpht.com/ytc/AAUvwngQ4bmzYvPJ1N0eh27Rl67yAOoydc8TPeLcPa_4Tw=s68-c-k-c0x00ffffff-no-rj" />
-                </div>
-                <div class="Video__Right">
-                    <h3>How Well Can an AI Learn Physics?</h3>
-                    <p>Two Minute Papers</p>
-                    <p>1.4M views • 11 months ago</p>
+                {/* Avatar */}
+                <div className="Video__Details">
+                    <div class="Video__ProfilePicture">
+                        <img src="https://yt3.ggpht.com/ytc/AAUvwngQ4bmzYvPJ1N0eh27Rl67yAOoydc8TPeLcPa_4Tw=s68-c-k-c0x00ffffff-no-rj" />
+                    </div>
+                    <div class="Video__Right">
+                        <h3>How Well Can an AI Learn Physics?</h3>
+                        <p>Two Minute Papers</p>
+                        <p>1.4M views • 11 months ago</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Button>
     )
 }
 
--- src/Videos.js
+++ src/Videos.js
@@ -5,32 +5,54 @@ import Video from './Video'
 function Videos() {
     return (
         <div className="Videos">
-            <div className="Section">
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
+            <div className="Videos__Topics">
+                <h3> All </h3>
+                <h3> Gordon Ramsay </h3>
+                <h3> Live </h3>
+                <h3> Deep House </h3>
+                <h3> Game shows </h3>
+                <h3> Music </h3>
+                <h3> Oceans </h3>
+                <h3> Cooking shows </h3>
+                <h3> Comedy </h3>
+                <h3> Chocolate </h3>
+                <h3> Eating </h3>
+                <h3> Nature </h3>
+                <h3> Terrestrial animals </h3>
+                <h3> Canis </h3>
+                <h3> Puppies </h3>
+                <h3> Driving </h3>
+                <h3> ReactJS </h3>
+                <h3> &gt; </h3>
             </div>
+            <div className="Videos__Stack">
+                <div className="Section">
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                </div>
 
-            <div className="Section">
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-            </div>
+                <div className="Section">
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                </div>
 
-            <div className="Section">
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-            </div>
+                <div className="Section">
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                </div>
 
-            <div className="Section">
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
-                <Video className="Videos__Flex" />
+                <div className="Section">
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                    <Video className="Videos__Flex" />
+                </div>
             </div>
 
         </div>
